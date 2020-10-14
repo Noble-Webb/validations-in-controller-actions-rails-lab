08d24aa499167c01ebd23855df431ebef9cc1676
Justin_Webb <Webb.n.justin@gmail.com>
2020-10-14 14:36:20 -0600

    Use a conditional statement in an action to render different responses based on the validation state of a model

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,9 +8,20 @@ class PostsController < ApplicationController
   end
 
   def update
-    @post.update(post_params)
+    # Run the validations WITHOUT saying valid post will only 
+    # update if done correctly
+    if @post.update(post_params)
 
-    redirect_to post_path(@post)
+      # This returns a status_code of 302, which instructs the browser to
+      # perform a NEW REQUEST! (AKA: throw @post away and let the show action
+      # worry about re-reading it from the database)
+      redirect_to post_path(@post)
+    else
+      # If the post is invalid, hold on to @post, because it is now full of
+      # useful error messages, and re-render the :edit page, which knows how
+      # to display them alongside the user's entries.
+      render :edit 
+    end
   end
 
   private
@@ -22,4 +33,6 @@ class PostsController < ApplicationController
   def set_post!
     @post = Post.find(params[:id])
   end
+
+  
 end
